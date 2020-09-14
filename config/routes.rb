fd5b091546fcb81e11a748d0257fcfc8b49fc268
Grace Teng <grace@pelicularities.net>
2020-09-14 17:44:00 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :pokemons do
     resources :rentals, only: [:create]
   end
-  resources :rentals do # we don't actually want any rentals routes, we just want to nest reviews inside rentals
+  resources :rentals, only: [] do # we don't actually want any rentals routes, we just want to nest reviews inside rentals
     resources :reviews, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
