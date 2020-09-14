a17c09fb712396aea8158a785d371111f78e508e
Grace Teng <grace@pelicularities.net>
2020-09-14 17:36:05 +0800

    created seeds for Pokedex

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
-  has_many :pokemons, :rentals
+  has_many :pokemons
+  has_many :rentals
   has_many :reviews, through: :rentals
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :rentals, only: [:create]
   end
   resources :rentals do # we don't actually want any rentals routes, we just want to nest reviews inside rentals
-    resources :reviews, only [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
