7acc44adad83748be187823aa298680685ecaba8
Grace Teng <grace@pelicularities.net>
2020-09-15 12:47:23 +0800

    wrote CUD actions for pokemons_controller

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,4 +1,5 @@
 class PokemonsController < ApplicationController
+<<<<<<< Updated upstream
     def new
     end
 
@@ -18,4 +19,45 @@ class PokemonsController < ApplicationController
     private
 
 
+=======
+  before_action :set_pokemon, only: [:edit, :update, :destroy]
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    pokemon = Pokemon.new(pokemon_params)
+    pokemon.user = current_user
+    pokemon.save!
+
+    redirect_to pokemon_path(pokemon)
+  end
+
+  def edit
+  end
+
+  def update
+    @pokemon.update(pokemon_params)
+    pokemon.save!
+
+    redirect_to pokemon_path(pokemon)
+  end
+
+  def destroy
+    @pokemon.destroy
+    
+    redirect_to pokemons_path
+  end
+
+  private
+
+  def set_pokemon
+    @pokemon = Pokemon.find(params[:id])
+  end
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :description, :price, :location, :pokedex)
+  end
+>>>>>>> Stashed changes
 end
