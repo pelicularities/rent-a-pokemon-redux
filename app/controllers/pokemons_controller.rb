99d7abe61e2e39463e778e16c4935111c37d3106
Grace Teng <grace@pelicularities.net>
2020-09-15 12:22:58 +0800

    generated Pokemons controller

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,21 @@
+class PokemonsController < ApplicationController
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+        
+    end
+
+    def destroy
+    end
+    
+    private
+
+
+end
