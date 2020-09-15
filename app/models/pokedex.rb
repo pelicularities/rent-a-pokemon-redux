399e4ba0d97d9c0b1740782658b0f39f99925b1c
Zack Xu <zack.xu@hotmail.com>
2020-09-15 15:54:08 +0800

    add activeStorage & updated seed for artwork uploading

--- app/models/pokedex.rb
+++ app/models/pokedex.rb
@@ -1,4 +1,5 @@
 class Pokedex < ApplicationRecord
-  has_many :pokemons
+  has_many :pokemons, dependent: :destroy
   validates :species, presence: true, uniqueness: true
+  has_one_attached :artwork, dependent: :destroy
 end
