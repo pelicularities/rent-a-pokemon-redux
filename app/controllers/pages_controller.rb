b0a7e92dfcb5e75e1f5b0d38195958aca3675e3c
Grace Teng <grace@pelicularities.net>
2020-09-17 13:16:15 +0800

    implemented display of rentals to and from other users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,19 +11,18 @@ class PagesController < ApplicationController
     user = current_user
     @rentals = {
       upcoming: {
-        from: user.rentals.where("start_date > ?", Time.now.to_date),
-        to: []
+        from: user.rentals.where("start_date > ?", Time.now.to_date).order(start_date: :asc),
+        to: Rental.joins(pokemon: :user).where(pokemons: { user: user }).where("start_date > ?", Time.now.to_date).order(start_date: :asc)
       },
       present: {
-        from: user.rentals.where("start_date <= ?", Time.now.to_date).where("end_date >= ?", Time.now.to_date),
-        to: []
+        from: user.rentals.where("start_date <= ?", Time.now.to_date).where("end_date >= ?", Time.now.to_date).order(start_date: :asc),
+        to: Rental.joins(pokemon: :user).where(pokemons: { user: user }).where("start_date <= ?", Time.now.to_date).where("end_date >= ?", Time.now.to_date).order(start_date: :asc)
       },
       past: {
-        from: user.rentals.where("end_date < ?", Time.now.to_date),
-        to: []
+        from: user.rentals.where("end_date < ?", Time.now.to_date).order(end_date: :desc),
+        to: Rental.joins(pokemon: :user).where(pokemons: { user: user }).where("end_date < ?", Time.now.to_date).order(end_date: :desc)
       }
     }
     # @rentals = user.rentals.all
-
   end
 end
