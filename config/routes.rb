5ae080250daf545c53e68b6ea567451f7963ef64
allen chung <east11@gmail.com>
2020-09-18 13:02:45 +0800

    success page partially done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :pokemons do
-    resources :rentals, only: [:new, :create]
+    resources :rentals, only: [:new, :create] do
+      collection do
+        get :success
+      end
+    end
   end
   resources :rentals, only: [] do # we don't actually want any rentals routes, we just want to nest reviews inside rentals
     resources :reviews, only: [:new, :create]
@@ -11,3 +15,5 @@ Rails.application.routes.draw do
   get '/history', to: 'pages#history'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+#  get '/pokemons/:pokemon_id/rentals/success', to: 'rentals#success'
